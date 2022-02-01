25689d9aff20de47dfaf0344a62f14f392fe084e
Olia Rosiak <olia.rosiak@gmail.com>
2022-02-01 22:14:47 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    bodyRef: document.querySelector('body'),
+    paragraphRef: document.querySelector('p')
+}
+let timerId = null;
+/**
+ *  Buttons design
+ */
+const wrapperRef = document.createElement('div');
+refs.paragraphRef.after(wrapperRef);
+wrapperRef.append(refs.startBtn, refs.stopBtn);
 
+wrapperRef.classList.add('wrapper');
+refs.startBtn.classList.add('btn', 'start');
+refs.stopBtn.classList.add('btn', 'stop', 'onstop');
+/**
+ *  Styles for buttons and body
+ */
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+    timerId = setInterval(() => {
+        refs.bodyRef.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    refs.startBtn.classList.add('onclick');
+    refs.stopBtn.classList.remove('onstop');
+}
+
+function onStopBtn() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled');
+    refs.startBtn.classList.remove('onclick');
+    refs.stopBtn.classList.add('onstop');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
